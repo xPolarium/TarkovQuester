25a5a311ad49717db108a20501f47f803cf4b12b
Miguel Torres <mikeajr@icloud.com>
2023-03-04 15:33:01 -0600

    create nodes based on some data given

--- src/components/QuestNode.tsx
+++ src/components/QuestNode.tsx
@@ -1,14 +1,15 @@
 interface QuestNodeProps {
   posX: number;
   posY: number;
+  quest:
+    | { name: string; wiki: string; type: string; objectives: string[] }
+    | undefined;
 }
 
-const QuestNode: React.FC<QuestNodeProps> = ({ posX, posY }) => {
-  const positionX = `top-[${posX}px]`;
-
+const QuestNode: React.FC<QuestNodeProps> = ({ posX, posY, quest }) => {
   // Tailwind builds classes before the client calculates dynamic values so use inline styles or CSS-in-JS library like Emotion to fix
   return (
-    <div className={`absolute ${positionX}`}>
+    <div className="absolute" style={{ top: `${posY}px`, left: `${posX}px` }}>
       <div className="relative h-min rounded-md border-2 border-neutral-500 bg-neutral-200 px-2 py-3">
         <div
           id="in"
@@ -19,14 +20,15 @@ const QuestNode: React.FC<QuestNodeProps> = ({ posX, posY }) => {
           className="absolute -right-[6px] -top-[6px] h-3 w-3 rounded-full bg-red-600"
         ></div>
         <div className="text-sm">
-          <h1 className="font-bold">Debut</h1>
-          <p>Wiki: https://escapefromtarkov.fandom.com/wiki/Debut</p>
-          <p>Type: Elimination</p>
+          <h1 className="font-bold">{quest?.name}</h1>
+          <p>Wiki: {quest?.wiki}</p>
+          <p>Type: {quest?.type}</p>
           <div>
             <p>Objectives:</p>
             <ul className="text-xs">
-              <li> - Eliminate 5 Scavs all over the Tarkov territory</li>
-              <li> - Obtain and hand over 2 MP-133 12ga shotguns</li>
+              {quest?.objectives.map((q, i) => (
+                <li key={`${i}-${quest?.name}`}>{q}</li>
+              ))}
             </ul>
           </div>
         </div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,6 +8,28 @@ import { api } from "~/utils/api";
 import QuestNode from "~/components/QuestNode";
 
 const Home: NextPage = () => {
+  const quests = [
+    {
+      name: "Debut",
+      wiki: "https://escapefromtarkov.fandom.com/wiki/Debut",
+      type: "Elimination",
+      objectives: [
+        "Eliminate 5 Scavs all over the Tarkov territory",
+        "Obtain and hand over 2 MP-133 12ga shotguns",
+      ],
+    },
+    {
+      name: "Search Mission",
+      wiki: "https://escapefromtarkov.fandom.com/wiki/Search_Mission",
+      type: "Completion",
+      objectives: [
+        "Find Prapor's missing convoy on Woods",
+        "Locate the temporary USEC camp",
+        "Survive and extract from the location",
+      ],
+    },
+  ];
+
   return (
     <>
       <Head>
@@ -18,9 +40,10 @@ const Home: NextPage = () => {
         />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
       <main className="relative flex h-screen">
-        <QuestNode posX={200} posY={300} />
-        <QuestNode posX={700} posY={90} />
+        <QuestNode posX={200} posY={240} quest={quests[0]} />
+        <QuestNode posX={700} posY={90} quest={quests[1]} />
       </main>
     </>
   );
